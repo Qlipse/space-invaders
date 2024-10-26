8e00548248ed3715787d67daa1a4001a3f973653
Qlipse <jackieliang1234@gmail.comm>
2024-10-26 17:18:06 -0400

    Added projectiles

--- index.js
+++ index.js
@@ -61,7 +61,31 @@ class Player {
   }
 }
 
+class Projectile {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+
+    this.radius = 3;
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.fillStyle = "red";
+    c.fill();
+    c.closePath();
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
+
 const player = new Player();
+const projectiles = [];
 const keys = {
   w: {
     pressed: false,
@@ -85,6 +109,16 @@ function animate() {
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
+  projectiles.forEach((projectile, index) => {
+    if (projectile.position.y + projectile.radius <= 0) {
+      setTimeout(() => {
+        projectiles.splice(index, 1);
+      }, 0);
+    } else {
+      projectile.update();
+    }
+  });
+
   if (keys.a.pressed && player.position.x >= 0) {
     player.velocity.x = -5;
     player.rotation = -0.15;
@@ -132,6 +166,19 @@ addEventListener("keydown", ({ key }) => {
     case " ":
       console.log("space");
       keys.space.pressed = true;
+      projectiles.push(
+        new Projectile({
+          position: {
+            x: player.position.x + player.width / 2,
+            y: player.position.y,
+          },
+          velocity: {
+            x: 0,
+            y: -10,
+          },
+        })
+      );
+      //console.log(projectiles);
       break;
   }
 });
